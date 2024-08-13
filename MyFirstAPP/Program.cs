4a6817c9925ac8a0cf66ca5daf7b9ea87d186327
Vilmar Viimsalu <vilmar.viimsalu@tktk.ee>
2024-08-13 15:08:07 +0300

    create new app

---
+++ MyFirstAPP/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello " + userName + "!"); //Output
+//string interpolation
+
+Console.WriteLine($"Hello {userName} !"); //Output
